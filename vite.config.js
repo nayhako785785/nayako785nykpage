319d538e6e4e2274acc2561b36943645fdc74072
nayhako785785 <nayako785785@DESKTOP-16JR7M7>
2025-03-22 14:59:54 +0900

    base change

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base:"https://nayhako785785.github.io/nayako785nykpage.github.io/"
 })
